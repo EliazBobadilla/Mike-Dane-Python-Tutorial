9a256a1bb7c328d61293f8b4d208c5c362c09707
Chrome OwO <Chrome@localhost.localdomain>
2020-12-21 12:33:12 -0500

    Commit

---
+++ 07_Numbers.py
@@ -0,0 +1,19 @@
+print(689*(478/89)+561)
+print(10%3)
+
+my_number = 7
+print(str(my_number) , "Is my favorite Number")
+
+myNumberTwo = -898
+print(abs(myNumberTwo))
+
+print(pow(4,98))
+
+print(max(5616556,465465456,5445,89489,84849,489498,4894))
+
+print(min(5616556,465465456,5445,89489,84849,489498,4894))
+
+print(round(45.57878978))
+
+#https://www.mikedane.com/programming-languages/python/math/
+myNumberThree = -45
