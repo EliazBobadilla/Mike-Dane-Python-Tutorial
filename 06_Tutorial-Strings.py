35d4faf63bca5e9b0b0d63ce8600c4a94e478cd4
Chrome <eliaz.bobadilladev@gmail.com>
2020-12-20 19:05:57 -0500

    Commit

---
+++ 06_Tutorial-Strings.py
@@ -0,0 +1,13 @@
+greeting = "Hello"
+#indexes:   01234 
+
+print( greeting[0] )
+print( len(greeting) )
+
+mike_dane = "is cool"
+print("Mike dane" , mike_dane)
+
+master = "Master is Master"
+print(master.index("i"))
+
+#https://www.mikedane.com/programming-languages/python/strings/
