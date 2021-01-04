b88b2084de2e5a417190569ab7973b6f2d04f550
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-01-03 21:51:02 -0500

    Final Commit(?

---
+++ 09.5_Calculator.py
@@ -0,0 +1,15 @@
+operacion = input("Escoge que quiere hacer:")
+
+if operacion == "Suma":
+   numeroSuma1 = input("Enter a number: ")
+   numerosuma2 = input("Enter another number: ")
+result = float(numeroSuma1) + float(numerosuma2)
+
+print(result)
+
+if operacion == "Resta":
+   numerounoresta = input("Enter a number: ")
+   numerodosresta = input("Enter another number: ")
+result = float(numerounoresta) - float(numerodosresta)
+
+print(result)
---
+++ 09_BulidingCalculator.py
@@ -0,0 +1,9 @@
+operacion = input("Escoge que quiere hace:")
+
+if operacion == "Suma":
+   numberone = input("Enter a number: ")
+   numbertwo = input("Enter another number: ")
+
+result = float(numberone) + float(numbertwo)
+
+print(result)
